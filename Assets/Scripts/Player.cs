01d22849fb3b9ad9d6c71c1a23853d60f68a1b34
Demii08 <166402786+Demii08@users.noreply.github.com>
2024-04-29 17:56:28 +0300

    Cinemachine and input refactor

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,31 +7,14 @@ using UnityEngine.UIElements;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 7f;
+    [SerializeField] private GameInput _gameInput;
     private float rotateSpeed;
 
     public bool isWalking;
 
     private void Update()
     {
-        Vector2 inputVector = new Vector2(0, 0);
-        if (Input.GetKey(KeyCode.W))
-        {
-            inputVector.y = +1;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            inputVector.y = -1;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            inputVector.x = -1;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            inputVector.x = +1;
-        }
-
-        inputVector = inputVector.normalized;
+        Vector2 inputVector = _gameInput.GetMovementVectorNormalized();
 
         
 
