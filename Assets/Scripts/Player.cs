fc542c75aec7d53d83339351714e163a6ff47077
Demii08 <166402786+Demii08@users.noreply.github.com>
2024-05-07 18:56:50 +0300

    Collision System

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -19,6 +19,60 @@ public class Player : MonoBehaviour
         
 
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+
+        float moveDistance = moveSpeed * Time.deltaTime;
+        float playerRadius = .7f;
+        float playerHeight = 2f;
+
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight,  playerRadius, moveDir, moveDistance);
+
+        if (!canMove) 
+        {
+            //cannot move through moveDir
+
+            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0).normalized;
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
+
+            if (canMove)
+            {
+                //Can move only on the X
+
+                moveDir = moveDirX;
+            } 
+
+            else
+
+            {
+                //Can move only on the X
+                //Atempt only Z movement
+
+                Vector3 moveDirZ = new Vector3(0, -0, moveDir.z).normalized;
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
+
+                if(canMove)
+                    //can move only on the Z
+                {
+                    moveDir = moveDirZ;
+                }
+                
+                else
+                {
+                    //cannot move in any direction
+
+
+                }
+
+            }
+
+
+
+        }
+        
+        if (canMove)
+        {
+            transform.position += moveDir * moveDistance;
+        }
+
         isWalking = (moveDir != Vector3.zero);
         transform.position += moveDir * (moveSpeed * Time.deltaTime);
 
